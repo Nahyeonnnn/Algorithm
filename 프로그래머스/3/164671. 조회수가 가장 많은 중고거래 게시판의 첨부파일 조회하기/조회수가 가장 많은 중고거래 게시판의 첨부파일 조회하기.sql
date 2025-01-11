e46aa06dce74fb552251f2dde705e1da84aa63c4
kim nahyeon <stome6322@inha.edu>
2025-01-11 10:51:18 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,7 @@
+SELECT concat('/home/grep/src/', f.BOARD_ID, '/', f.FILE_ID, f.FILE_NAME, f.FILE_EXT) as FILE_PATH
+from USED_GOODS_FILE f join (
+    select BOARD_ID
+    from USED_GOODS_BOARD
+    order by VIEWS desc limit 1
+) b on b.BOARD_ID = f.BOARD_ID
+order by f.FILE_ID desc
