c4684dd407f2099aa5d3f8a203b146b28e05fd5b
kim nahyeon <stome6322@inha.edu>
2025-03-01 13:23:06 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n,k;
+    cin >> n >> k;
+    vector<vector<long long>> dp(n+1, vector<long long>(k+1));
+
+    for (int i=1; i<=k; i++) {
+        dp[1][i] = i;
+    }
+    for (int i=1; i<=n; i++) {
+        dp[i][1] = 1;
+    }
+
+    for (int i=2; i<=n; i++) {
+        for (int j=2; j<=k; j++) {
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
+        }
+    }
+
+    cout << dp[n][k];
+
+    return 0;
+}
