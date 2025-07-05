3d5659f14c861b1295b4b977c24bd0f7da2ea129
kim nahyeon <stome6322@inha.edu>
2025-07-05 11:49:36 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,7 +1,15 @@
-SELECT p.ID, p.NAME, p.HOST_ID
-from PLACES p join (
-    select host_id, count(host_id) as count_host from PLACES
-    group by host_id
-) s on p.host_id = s.host_id
-where 2 <= s.count_host
+# SELECT p.ID, p.NAME, p.HOST_ID
+# from PLACES p join (
+#     select host_id, count(host_id) as count_host from PLACES
+#     group by host_id
+# ) s on p.host_id = s.host_id
+# where 2 <= s.count_host
 
+
+select p.id, p.name, p.host_id
+from PLACES p join (select count(host_id) as cnt, host_id
+       from places 
+       group by host_id
+      ) h on p.host_id=h.host_id
+where h.cnt > 1
+order by id
